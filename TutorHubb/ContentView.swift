9121a2c75a492e68eeb899cfa1ca2951b730678a
Bolanle Adisa <bolanleadisa6718@gmail.com>
2023-11-11 16:13:05 -0600

    Initial Commit

---
+++ TutorHubb/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TutorHubb
+//
+//  Created by Bolanle Adisa on 11/11/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TutorHubb/TutorHubbApp.swift
@@ -0,0 +1,17 @@
+//
+//  TutorHubbApp.swift
+//  TutorHubb
+//
+//  Created by Bolanle Adisa on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TutorHubbApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
