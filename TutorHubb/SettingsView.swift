fe687edc47800c5bb55ab1446a9266fdbca54641
Bolanle Adisa <bolanleadisa6718@gmail.com>
2023-11-21 16:15:17 -0600

    improved settings view

--- TutorHubb/SettingsView.swift
+++ TutorHubb/SettingsView.swift
@@ -1,26 +1,41 @@
-//
-//  SettingsView.swift
-//  TutorHubb
-//
-//  Created by Bolanle Adisa on 11/11/23.
-//
-
 import SwiftUI
 
 struct SettingsView: View {
     @ObservedObject var settings: SettingsModel
+    @State private var enableNotifications: Bool = true
+    @State private var weeklySummary: Bool = false
+    @State private var darkMode: Bool = false
+    @State private var language: String = "English"
     
     var body: some View {
         NavigationView {
             Form {
-                Toggle(isOn: $settings.isHighContrastEnabled) {
-                    Text("High Contrast Mode")
+                Section(header: Text("Personal Preferences")) {
+                    Toggle(isOn: $enableNotifications) {
+                        Text("Enable Notifications")
+                    }
+                    Toggle(isOn: $weeklySummary) {
+                        Text("Weekly Summary Emails")
+                    }
+                    Picker("Language", selection: $language) {
+                        Text("English").tag("English")
+                        Text("Spanish").tag("Spanish")
+                        // Add more languages as needed
+                    }
                 }
-                
-                Stepper("Font Size: \(Int(settings.selectedFontSize))", value: $settings.selectedFontSize, in: 12...24)
+
+                Section(header: Text("App Settings")) {
+                    Toggle(isOn: $darkMode) {
+                        Text("Dark Mode")
+                    }
+                    Toggle(isOn: $settings.isHighContrastEnabled) {
+                        Text("High Contrast Mode")
+                    }
+                    Stepper("Font Size: \(Int(settings.selectedFontSize))", value: $settings.selectedFontSize, in: 12...24)
+                }
             }
-            .navigationTitle("Settings")
+            .navigationBarTitle("Settings", displayMode: .inline)
+            .listStyle(GroupedListStyle())
         }
     }
 }
-
